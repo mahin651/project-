c23b5582ffd104cf45793ee71107c7d90c0e1cd6
Your Name <mahinmittal2018@gmail.com>
2022-05-18 12:06:41 +0530

    added email file

--- Online-Bid-Items-NodeJS/models/category.js
+++ Online-Bid-Items-NodeJS/models/category.js
@@ -1,5 +1,5 @@
 const mongoose = require('./db.js');
-const Schema = mongoose.Schema;
+    const Schema = mongoose.Schema;
 
 const categorySchema = new Schema({
     name: { type:String, required: true, default: 'default'}
